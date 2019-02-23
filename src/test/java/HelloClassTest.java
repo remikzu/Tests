b8410c6052b1bb6b0aa0472836b7f2e63f2707e0
Remigiusz Zudzin <remik.zudzin@gmail.com>
2019-02-23 09:55:59 +0100

    Testy z lambdami

--- src/test/java/HelloClassTest.java
+++ src/test/java/HelloClassTest.java
@@ -1,5 +1,9 @@
+import com.sun.org.glassfish.gmbal.Description;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Remigiusz Zudzin
@@ -7,17 +11,24 @@ import org.junit.jupiter.api.Test;
 class HelloClassTest {
     private static HelloClass helloClass = new HelloClass();
     @Test
+    @DisplayName("Nazwa testu 1")
+    @Description("Opis do testu 1")
     void HelloTest() {
-        Assertions.assertEquals("Hello World!", helloClass.getMessage());
+        Assertions.assertAll("Asercje do HelloWorld",
+                () -> assertEquals("Hello World2!", helloClass.getMessage()),
+                () -> assertEquals("Cos innego", helloClass.getMessage()));
     }
 
-    @Test
-    void Test2() {
-        Assertions.assertEquals("HLO WRLD", helloClass.getMessage());
-    }
-    @Test
-    void Test3() {
-        Assertions.assertEquals("Cos innego", helloClass.getMessage());
-    }
+//    @Test
+//    @DisplayName("Nazwa testu 2")
+//    @Description("Opis do testu 2")
+//    void Test2() {
+//
+//    }
+//    @Test
+//    @DisplayName("Nazwa testu 3")
+//    @Description("Opis do testu 3")
+//    void Test3() {
+//    }
 
 }
